506f92ad6d4fee4589116c181220196419ba7a6f
Adam <adamkrupa96@gmail.com>
2019-04-22 20:57:34 +0200

    add initial commit with dev and prod configuration

---
+++ backend/src/main/java/com/platform/geolocations/GeolocationsPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.platform.geolocations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeolocationsPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeolocationsPlatformApplication.class, args);
+	}
+
+}
